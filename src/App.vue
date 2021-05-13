59bb3fb19fb08af56504d0f8c1e3e3bcb4144ea4
= <=>
2021-05-14 01:47:34 +0300

    App init|Create header

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Header />
+    <div class="container">
+      Test
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/layouts/Header";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./public/css/_style.scss";
+</style>
---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <img src="" alt="logo" />
+    <div class="action-btns">
+      <div class="theme-switcher-wrap">
+        <div class="theme-switcher">
+          <div class="switch-path">
+            <div class="switch-handle"></div>
+          </div>
+        </div>
+      </div>
+      <button>Create</button>
+    </div>
+  </header>
+</template>
